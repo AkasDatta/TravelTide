d7dfc30d8df8c15f56e8df1e35f33ff050bb1448
Akas_Datta <012akasdatta@gmail.com>
2023-11-19 03:49:20 +0600

    some responsiveness added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
     <div>
         <NavbarTop></NavbarTop>
         <div className='bg-white shadow'>
-      <nav className="p-3 md:flex md:items-center md:justify-between max-w-screen-2xl mx-auto lg:px-8 overflow-x-hidden">
+      <nav className="p-3 md:flex md:items-center md:justify-between max-w-screen-2xl mx-auto lg:px-4 overflow-x-hidden">
         <div className="flex justify-between items-center">
-          <Link to="/">
+          <Link to="/" className='my-4'>
             <img src={logo} alt="" />
           </Link>
 
@@ -29,52 +29,57 @@ const Navbar = () => {
             {menuOpen ? <FaBars/> : <FaBars/>}
           </span>
         </div>
-
-        <ul
-          className={`md:flex md:items-center z-[+1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 ${
-            menuOpen ? 'opacity-100 top-[80px]' : 'opacity-0 top-[-400px]'
-            } transition-all ease-in duration-500`}
-        >
-
-          <li className="mx-2 my-3 md:my-0">
-            <Link to="/" className="text-sm text-gray-600  hover:text-blue-600 duration-600">
-              Home
-            </Link>
-          </li>
-          <li className="mx-2 my-3 md:my-0">
-            <Link to="/colleges" className="text-sm text-gray-600 hover:text-blue-600 duration-600">
-              Colleges
-            </Link>
-          </li>
-          <li className="mx-2 my-3 md:my-0">
-            <Link to="/admission" className="text-sm text-gray-600 hover:text-blue-600 duration-600">
-              Admission
-            </Link>
-          </li>
-          {/* {user && ( */}
-            <li className="mx-2 my-3 md:my-0">
-              <Link to="/mycollege" className="text-sm text-gray-600 hover:text-blue-600 duration-600">
-                My College
-              </Link>
-            </li>
-          {/* )} */}
-
-          {/* <div className="form-control mx-2 sm:my-5">
-            <input type="text" placeholder="Search" className="border border-gray-300 rounded-md bg-white w-36 px-2 py-1" />
-          </div> */}
-
-            <div className="mx-2 my-3 md:my-0">
-                <FaBasketShopping className='text-2xl text-gray-700'></FaBasketShopping>
+            <div className={`md:flex md:items-center z-[+1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 ${menuOpen ? 'opacity-100 top-[80px]' : 'opacity-0 top-[-400px]'} transition-all ease-in duration-500`}>
+                <ul className='md:flex lg:mx-28 md:mx-10'>
+                    <li className="lg:mx-10 md:mx-2 my-3 md:my-0">
+                        <Link to="/" className="lg:text-lg md:text-md text-black hover:text-teal-500 font-bold duration-600">
+                        Home
+                        </Link>
+                    </li>
+                    <li className="lg:mx-10 md:mx-2 my-3 md:my-0">
+                        <Link to="/colleges" className="lg:text-lg md:text-md text-black hover:text-teal-500 font-bold duration-600">
+                        Pages
+                        </Link>
+                    </li>
+                    <li className="lg:mx-10 md:mx-2 my-3 md:my-0">
+                        <Link to="/admission" className="lg:text-lg md:text-md text-black hover:text-teal-500 font-bold duration-600">
+                        About
+                        </Link>
+                    </li>
+                    <li className="lg:mx-10 md:mx-2 my-3 md:my-0">
+                        <Link to="/mycollege" className="lg:text-lg md:text-md text-black hover:text-teal-500 font-bold duration-600">
+                            Tours
+                        </Link>
+                    </li>
+                    <li className="lg:mx-10 md:mx-2 my-3 md:my-0">
+                        <Link to="/mycollege" className="lg:text-lg md:text-md text-black hover:text-teal-500 font-bold duration-600">
+                            Blogs
+                        </Link>
+                    </li>
+                    <li className="lg:mx-10 md:mx-2 my-3 md:my-0">
+                        <Link to="/mycollege" className="lg:text-lg md:text-md text-black hover:text-teal-500 font-bold duration-600">
+                            Shop
+                        </Link>
+                    </li>
+                    <li className="lg:mx-10 md:mx-2 my-3 md:my-0">
+                        <Link to="/mycollege" className="lg:text-lg md:text-md text-black hover:text-teal-500 font-bold duration-600">
+                        Destributions
+                        </Link>
+                    </li>
+                </ul>
+                <div className='flex mt-3'>
+                    <div className="lg:mx-4 md:mx-2 md:my-0">
+                            <FaBasketShopping className='text-2xl text-gray-700'></FaBasketShopping>
+                        </div>
+                        <div className="lg:mx-4 md:mx-4 md:my-0">
+                            <FaSistrix className='text-2xl'></FaSistrix>
+                        </div>
+                        <div className="lg:mx-4 md:mx-2 md:my-0">
+                            <FaRegCircleUser className='text-4xl'></FaRegCircleUser>
+                        </div>
+                </div>
             </div>
-            <div className="mx-2 my-3 md:my-0">
-                <FaSistrix className='text-2xl'></FaSistrix>
-            </div>
-            <li className="mx-2 my-3 md:my-0">
-                <FaRegCircleUser className='text-4xl'></FaRegCircleUser>
-            </li>
-
          
-        </ul>
       </nav>
     </div>
     </div>
