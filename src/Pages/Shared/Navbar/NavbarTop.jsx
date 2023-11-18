95055aa6e9bec26ffc8f620397367fb2585f89b6
Akas_Datta <012akasdatta@gmail.com>
2023-11-18 18:41:13 +0600

    react icon added and navbartop some text and design added

--- src/Pages/Shared/Navbar/NavbarTop.jsx
+++ src/Pages/Shared/Navbar/NavbarTop.jsx
@@ -1,12 +1,25 @@
+import { GoMail } from "react-icons/go";
+import { FaPhone } from "react-icons/fa";
 
 const NavbarTop = () => {
     return (
         <div>
             <div className="navbar bg-base-100">
-                <div className="flex-none text-teal-400">
-                    <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                    </button>
+                <div className="flex-none text-white hover:text-teal-400 cursor-pointer">
+                    <div className="mx-3 flex">
+                        <GoMail className="mx-4 mt-1.5 text-xl text-teal-400"></GoMail>
+                        <div>
+                            <h2 className="text-lg">akasdatta.me@gmail.com</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-none text-white hover:text-teal-400 cursor-pointer">
+                    <div className="mx-3 flex">
+                        <FaPhone className="mx-4 mt-1.5 text-xl text-teal-400"></FaPhone>
+                        <div>
+                            <h2 className="text-lg">+880 1810 318499</h2>
+                        </div>
+                    </div>
                 </div>
                 <div className="flex-1">
                     <a className="btn btn-ghost text-xl  text-teal-400">daisyUI</a>
