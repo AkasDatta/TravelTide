030d67fedcef3febb432e47b3bb9768ef89b7510
Akas_Datta <012akasdatta@gmail.com>
2023-11-23 14:45:19 +0600

    zooming and opacity changed

--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import banner from '../../../../assets/banner2.jpg';
+import banner from '../../../../assets/banner3.jpg';
+import './Banner.css';
 
 const Banner = () => {
   return (
-    <div
-      className="relative bg-cover bg-center h-screen flex items-center justify-center zoom-animation"
-      style={{
-        backgroundImage: `url(${banner})`,
-      }}
-    >
-      <div className="absolute inset-0 bg-gradient-to-b from-gray-900 to-gray-800 opacity-70"></div>
-      <div className="text-gray-300 text-4xl font-bold z-10">
-        Your Centered Text Here
+    <div className="relative h-screen overflow-hidden">
+      <div
+        className="bg-cover bg-center w-full h-full flex items-center justify-center zoom-animation"
+        style={{
+          backgroundImage: `url(${banner})`,
+        }}
+      >
+        <div className="absolute inset-0 bg-gradient-to-b from-gray-900 to-gray-700 opacity-50"></div>
+        <div className="text-gray-200 text-4xl font-bold z-10">
+          Your Centered Text Here
+        </div>
       </div>
     </div>
   );
