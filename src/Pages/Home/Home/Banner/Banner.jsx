814ad81f804b8488e9c37e05fc5147888b508f4e
Akas_Datta <012akasdatta@gmail.com>
2023-11-23 14:38:46 +0600

    bg image hover added

--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -1,14 +1,16 @@
-import banner from '../../../../assets/banner.jpg';
+import React from 'react';
+import banner from '../../../../assets/banner2.jpg';
 
 const Banner = () => {
   return (
     <div
-      className="bg-cover bg-center h-screen flex items-center justify-center"
+      className="relative bg-cover bg-center h-screen flex items-center justify-center zoom-animation"
       style={{
         backgroundImage: `url(${banner})`,
       }}
     >
-      <div className="text-white text-4xl font-bold">
+      <div className="absolute inset-0 bg-gradient-to-b from-gray-900 to-gray-800 opacity-70"></div>
+      <div className="text-gray-300 text-4xl font-bold z-10">
         Your Centered Text Here
       </div>
     </div>
