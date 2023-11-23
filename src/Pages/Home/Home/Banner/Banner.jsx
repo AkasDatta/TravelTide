af8e8388c99dc3239b3fe03b0207fc64612fd150
Akas_Datta <012akasdatta@gmail.com>
2023-11-24 02:01:39 +0600

    responsiveness changed banner text

--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -11,11 +11,11 @@ const Banner = () => {
         }}
       >
         <div className="absolute inset-0 bg-gradient-to-b from-black to-gray-700 opacity-60"></div>
-        <div className="text-gray-200 text-5xl font-bold z-10 flex flex-col items-center justify-center">
-          <span className='text-center banner-text'>
-            Where will you go next?
+        <div className="text-gray-200 font-bold z-10 flex flex-col items-center justify-center pb-20">
+          <span className='text-center banner-text py-10'>
+            <h2 className='xl:text-6xl lg:text-6xl md:text-5xl text-2xl'>Where will you go next?</h2>
           </span>
-          <p className='text-sm mx-20 my-10 text-center px-80 banner-text'>
+          <p className='xl:text-xl lg:text-xl md:text-xl text-sm mx-4 my-2 text-center xl:px-80 lg:px-60 md:px-40 px-4 banner-text'>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium laboriosam magni vero? Nihil sapiente doloribus modi a odio deleniti! At quae illum amet praesentium, facilis libero suscipit ut tempora optio et doloremque minus. Quam?
           </p>
         </div>
