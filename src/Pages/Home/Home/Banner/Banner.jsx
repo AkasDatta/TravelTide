8066738a36e07f7049583699cb3d81c749c478c1
Akas_Datta <012akasdatta@gmail.com>
2023-12-13 22:13:37 +0600

    navbar some responsive added

--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -1,25 +1,29 @@
 import banner from '../../../../assets/banner3.jpg';
+import BannerSearch from '../BannerSearch/BannerSearch';
 import './Banner.css';
 
 const Banner = () => {
   return (
-    <div className="relative h-screen overflow-hidden">
-      <div
-        className="bg-cover bg-center w-full h-full flex items-center justify-center zoom-animation"
-        style={{
-          backgroundImage: `url(${banner})`,
-        }}
-      >
-        <div className="absolute inset-0 bg-gradient-to-b from-black to-gray-700 opacity-60"></div>
-        <div className="text-gray-200 font-bold z-10 flex flex-col items-center justify-center pb-20">
-          <span className='text-center banner-text py-10'>
-            <h2 className='xl:text-6xl lg:text-6xl md:text-5xl text-2xl'>Where will you go next?</h2>
-          </span>
-          <p className='xl:text-xl lg:text-xl md:text-xl text-sm mx-4 my-2 text-center xl:px-80 lg:px-60 md:px-40 px-4 banner-text'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium laboriosam magni vero? Nihil sapiente doloribus modi a odio deleniti! At quae illum amet praesentium, facilis libero suscipit ut tempora optio et doloremque minus. Quam?
-          </p>
+    <div>
+      <div className="relative h-screen overflow-hidden">
+        <div
+          className="bg-cover bg-center w-full h-full flex items-center justify-center zoom-animation"
+          style={{
+            backgroundImage: `url(${banner})`,
+          }}
+        >
+          <div className="absolute inset-0 bg-gradient-to-b from-black to-gray-700 opacity-60"></div>
+          <div className="text-gray-200 font-bold z-10 flex flex-col items-center justify-center pb-20">
+            <span className='text-center banner-text py-10'>
+              <h2 className='xl:text-6xl lg:text-6xl md:text-5xl text-2xl'>Where will you go next?</h2>
+            </span>
+            <p className='xl:text-xl lg:text-xl md:text-xl text-sm mx-4 my-2 text-center xl:px-80 lg:px-60 md:px-40 px-4 banner-text'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium laboriosam magni vero? Nihil sapiente doloribus modi a odio deleniti! At quae illum amet praesentium, facilis libero suscipit ut tempora optio et doloremque minus. Quam?
+            </p>
+          </div>
         </div>
       </div>
+    <BannerSearch></BannerSearch>
     </div>
   );
 };
--- src/Pages/Home/Home/BannerSearch/BannerSearch.jsx
+++ src/Pages/Home/Home/BannerSearch/BannerSearch.jsx
@@ -1,8 +1,27 @@
 
 const BannerSearch = () => {
     return (
-        <div>
-            <h2>bannersearch</h2>
+        <div className="py-5">
+            <div className="container mx-auto border-3">
+                <form className="grid grid-cols-4 gap-4 border-3">
+                    <div className="col-span-1 border-3">
+                        <h1 className="text-3xl font-bold">Private Lessons</h1>
+                    </div>
+                    <div className="col-span-1">
+                        <h4>We offer music education for individuals of just about every age and skill level.</h4>
+                    </div>
+                    <div className="col-span-1">
+                        <button className="bg-blue-500 text-white rounded-full px-5 py-2 mt-3">
+                            Start Learning
+                        </button>
+                    </div>
+                    <div className="col-span-1">
+                        <button className="bg-blue-500 text-white rounded-full px-5 py-2 mt-3">
+                            Start Learning
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
