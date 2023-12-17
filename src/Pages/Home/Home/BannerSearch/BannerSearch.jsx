86ceaad8ea0199a90e571fe948f193930cd67f56
Akas_Datta <012akasdatta@gmail.com>
2023-12-18 01:21:57 +0600

    border added and input field added

--- src/Pages/Home/Home/BannerSearch/BannerSearch.jsx
+++ src/Pages/Home/Home/BannerSearch/BannerSearch.jsx
@@ -1,18 +1,38 @@
 
 const BannerSearch = () => {
     return (
-        <div className="py-5">
+        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-4">
             <div className="container mx-auto border-3">
-                <form className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-1 gap-4 border-3">
-                    <div className="col-span-1 border-3">
-                        <h1 className="text-3xl font-bold">Where to?</h1>
-                    </div>
-                    <div className="col-span-1">
-                        <h4>Month</h4>
-                    </div>
-                    <div className="col-span-1">
-                        <h4>Travel Type</h4>
-                    </div>
+                <form className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-1  border-3 shadow-lg">
+                        <div>
+                            <label className="sr-only" htmlFor="email">Email</label>
+                            <input
+                                className="w-full border p-7 text-sm bg-white"
+                                placeholder="Email address"
+                                type="email"
+                                id="email"
+                            />
+                        </div>
+
+                        <div>
+                            <label className="sr-only" htmlFor="phone">Phone</label>
+                            <input
+                                className="w-full border bg-white p-7 text-sm"
+                                placeholder="Phone Number"
+                                type="tel"
+                                id="phone"
+                            />
+                        </div>
+                        <div>
+                            <label className="sr-only" htmlFor="phone">Phone</label>
+                            <input
+                                className="w-full border bg-white p-7 text-sm"
+                                placeholder="Phone Number"
+                                type="tel"
+                                id="phone"
+                            />
+                        </div>
+
                     <div className="col-span-1">
                         <button className="bg-blue-500 text-white px-5 py-2 mt-3">
                             Find Now
