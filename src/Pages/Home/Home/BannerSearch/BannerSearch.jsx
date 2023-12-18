2c8d624aab54e1f074461db559780949ccc8802b
Akas_Datta <012akasdatta@gmail.com>
2023-12-18 22:45:28 +0600

    json data added

--- src/Pages/Home/Home/BannerSearch/BannerSearch.jsx
+++ src/Pages/Home/Home/BannerSearch/BannerSearch.jsx
@@ -1,51 +1,56 @@
-
 const BannerSearch = () => {
     return (
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-4" style={{ marginTop: '-50px' }}>
-            <div className="container mx-auto border-3">
-                <form className="grid grid-cols-1 lg:grid-cols-7 md:grid-cols-7 sm:grid-cols-1 border-3 shadow-lg">
-                        <div className="col-span-2 ">
-                            <label className="sr-only" htmlFor="email">Email</label>
-                            <input
-                                className="w-full border border-gray-100 p-7 text-sm bg-white"
-                                placeholder="Email address"
-                                type="email"
-                                id="email"
-                            />
-                        </div>
-
-                        <div className="col-span-2 ">
-                            <label className="sr-only" htmlFor="phone">Phone</label>
-                            <input
-                                className="w-full border border-gray-100 bg-white p-7 text-sm"
-                                placeholder="Phone Number"
-                                type="tel"
-                                id="phone"
-                            />
-                        </div>
-                        <div className="col-span-2 ">
-                            <label className="sr-only" htmlFor="phone">Phone</label>
-                            <input
-                                className="w-full border border-gray-100 bg-white p-7 text-sm"
-                                placeholder="Phone Number"
-                                type="tel"
-                                id="phone"
-                            />
-                        </div>
-
-                        <div className="col-span-1">
-                            {/* <button className="bg-teal-400 text-white px-12 py-7"> */}
-                            <button
-                                type="submit"
-                                className="inline-block w-full bg-teal-400 xl:px-20 lg:px-9 md:px-7 xl:py-5 lg:py-8 md:py-5 py-7 text-sm text-white sm:w-auto"
-                                >
-                                FIND NOW
-                            </button>
-                        </div>
-                </form>
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-4" style={{ marginTop: '-50px' }}>
+        <div className="container mx-auto border-3">
+          <form className="grid grid-cols-1 lg:grid-cols-7 md:grid-cols-7 sm:grid-cols-1 border-3 shadow-lg">
+            <div className="col-span-2">
+              <label className="sr-only" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="w-full border border-gray-100 p-7 text-sm bg-white focus:border-white focus:outline-none"
+                placeholder="Where to?"
+                type="email"
+                id="email"
+              />
             </div>
+  
+            <div className="col-span-2">
+              <label className="sr-only" htmlFor="phone">
+                Phone
+              </label>
+              <input
+                className="w-full border border-gray-100 bg-white p-7 text-sm focus:border-white focus:outline-none"
+                placeholder="Date"
+                type="date"
+              />
+            </div>
+  
+            <div className="col-span-2">
+              <label className="sr-only" htmlFor="phone">
+                Phone
+              </label>
+              <input
+                className="w-full border border-gray-100 bg-white p-7 text-sm focus:border-white focus:outline-none"
+                placeholder="Travel Type"
+                type="tel"
+                id="phone"
+              />
+            </div>
+  
+            <div className="col-span-1">
+              <button
+                type="submit"
+                className="inline-block w-full bg-teal-400 xl:px-20 lg:px-9 md:px-7 xl:py-5 lg:py-8 md:py-5 py-7 text-sm text-white sm:w-auto"
+              >
+                FIND NOW
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     );
-};
-
-export default BannerSearch;
+  };
+  
+  export default BannerSearch;
+  
--- src/Pages/Home/Home/HolidayCart/HolidayCart.jsx
+++ src/Pages/Home/Home/HolidayCart/HolidayCart.jsx
@@ -10,6 +10,37 @@ const HolidayCart = () => {
         <h1 className="font-bold text-6xl text-gray-800">Perfect Holiday</h1>
         <p className="mt-4 text-lg text-gray-700">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam <br /> facere quas porro id voluptas rem ab dolore sapiente repellat nemo.</p>
       </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 my-16">
+        <div>
+        <a href="#" className="group relative block overflow-hidden">
+            <article className="relative overflow-hidden transition hover:shadow-lg">
+            <img
+                alt="Office"
+                src="https://images.unsplash.com/photo-1587222318667-31212ce2828d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="absolute inset-0 h-full w-full object-cover transition duration-1000 group-hover:scale-105 sm:h-72"
+            />
+
+            <div className="relative pt-32 sm:pt-48 lg:pt-48">
+                <div className="p-4 sm:p-6">
+                    <h2 className="block text-2xl font-bold text-white"> Madrid </h2>
+                    <div className="flex items-center justify-between text-white font-bold">
+                        <div className="flex gap-2">
+                            <p>@</p>
+                            <p>5.0</p>
+                            <p>Good</p>
+                        </div>
+                        <div className="text-right text-xl">
+                            <h2>$3456</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </article>
+            </a>
+        </div>
+        
+      </div>
     </div>
   );
 };
