3ee537a98261771dfab4986ead26f368d42417f3
Akas_Datta <012akasdatta@gmail.com>
2023-12-18 18:04:07 +0600

    added holidaycart component logo and some text

--- src/Pages/Home/Home/HolidayCart/HolidayCart.jsx
+++ src/Pages/Home/Home/HolidayCart/HolidayCart.jsx
@@ -1,13 +1,17 @@
-
+import holidayLogo from "../../../../assets/logoholiday.png"
 
 const HolidayCart = () => {
-    return (
-        <div className="">
-            <h2>Choose Your</h2>
-            <h1>Perfect Holiday</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam facere quas porro id voluptas rem ab dolore sapiente repellat nemo.</p>
+  return (
+    <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-4 pt-28">
+      <div className="text-center">
+        <div className="flex justify-center items-center">
+          <img src={holidayLogo} alt="" />
         </div>
-    );
+        <h1 className="font-bold text-6xl text-gray-800">Perfect Holiday</h1>
+        <p className="mt-4 text-lg text-gray-700">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam <br /> facere quas porro id voluptas rem ab dolore sapiente repellat nemo.</p>
+      </div>
+    </div>
+  );
 };
 
 export default HolidayCart;
