e268fa4c55c6da86cba66512f029e44a4b0283ea
Akas_Datta <012akasdatta@gmail.com>
2023-12-18 17:34:51 +0600

    holidaycart component added

---
+++ src/Pages/Home/Home/HolidayCart/HolidayCart.jsx
@@ -0,0 +1,13 @@
+
+
+const HolidayCart = () => {
+    return (
+        <div className="">
+            <h2>Choose Your</h2>
+            <h1>Perfect Holiday</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam facere quas porro id voluptas rem ab dolore sapiente repellat nemo.</p>
+        </div>
+    );
+};
+
+export default HolidayCart;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner/Banner";
+import HolidayCart from "./HolidayCart/HolidayCart";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <HolidayCart></HolidayCart>
         </div>
     );
 };
