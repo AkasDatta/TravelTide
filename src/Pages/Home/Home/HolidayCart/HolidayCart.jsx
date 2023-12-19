199636aef8de2f98192a1952a67a12ec88101f84
Akas_Datta <012akasdatta@gmail.com>
2023-12-20 01:55:51 +0600

    card item responsive changed

--- src/Pages/Home/Home/HolidayCart/HolidayCart.jsx
+++ src/Pages/Home/Home/HolidayCart/HolidayCart.jsx
@@ -30,8 +30,25 @@ const HolidayCart = () => {
             {country.states.map((state, stateIndex) => (
               <a key={state.name} href="#" className={`group relative block overflow-hidden w-full ${stateIndex === 0 && (countryIndex === 0 || countryIndex === 2) ? 'rounded-full' : ''}`}>
                 <article className="relative overflow-hidden transition hover:shadow-lg">
-                  {stateIndex === 4 ? (
-                    <></>
+                  {stateIndex === 0 ? (
+                     <>
+                     <img
+                       alt={state.name}
+                       src={state.image}
+                       className="absolute inset-0 h-full w-full object-cover transition duration-1000 group-hover:scale-105 sm:h-96"
+                     />
+                     <div className="relative pt-32 md:pt-36 lg:pt-48 xl:pt-32">
+                       <h2 className="mx-auto text-center text-4xl mb-12 md:mb-16 xl:mb-16 text-white font-bold">{state.countryName}</h2>
+                       <div className="p-4 sm:p-6">
+                         <h2 className="block text-2xl font-bold text-white">{state.name}</h2>
+                         <div className="flex items-center justify-between text-white font-bold">
+                           <div className="text-right text-xl mb-8">
+                             <h2>{state.doller}{state.statePrice}</h2>
+                           </div>
+                         </div>
+                       </div>
+                     </div>
+                   </>
                   ) : (
                     <>
                       <img
@@ -40,6 +57,7 @@ const HolidayCart = () => {
                         className="absolute inset-0 h-full w-full object-cover transition duration-1000 group-hover:scale-105 sm:h-96"
                       />
                       <div className="relative pt-32 sm:pt-48 lg:pt-48">
+                        <h2 className="mx-auto text-center text-3xl text-white font-bold">{state.countryName}</h2>
                         <div className="p-4 sm:p-6">
                           <h2 className="block text-2xl font-bold text-white">{state.name}</h2>
                           <div className="flex items-center justify-between text-white font-bold">
