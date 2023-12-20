2cd38270f5aaf7673f3736bf4bb53f82fa8d2cb1
Akas_Datta <012akasdatta@gmail.com>
2023-12-21 02:25:13 +0600

    some react icon added and other added

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,13 +1,41 @@
+import { FaPhone } from "react-icons/fa";
 import logo from "../../../../src/assets/logo.png"
+import { GoMail } from "react-icons/go";
+import { FaLocationDot } from "react-icons/fa6";
 const Footer = () => {
     return (
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-4 pt-28 bg-[#212121]">
-            <div className=" text-white grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
+        <div className="bg-[#212121]">
+            <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-4 pt-28">
+                <div className="footer text-white grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
             <div>
-                <img src={logo} alt="" />
-                <p className="mx-14">ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+                <img className="mx-6" src={logo} alt="" />
+                <p className="mx-14">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ratione ut atque!</p>
+                <div className="flex-none text-white hover:text-teal-400 cursor-pointer">
+                    <div className="mx-3 flex">
+                        <GoMail className="xl:mx-4 lg:mx-1 mt-1.5 text-xl text-teal-400"></GoMail>
+                        <div>
+                            <h2 className="xl:text-lg lg:text-md">akasdatta.me@gmail.com</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-none text-white hover:text-teal-400 cursor-pointer">
+                    <div className="mx-3 flex">
+                        <FaPhone className="xl:mx-4 lg:mx-1 mt-1.5 text-xl text-teal-400"></FaPhone>
+                        <div>
+                            <h2 className="xl:text-lg lg:text-md">+880 1810 318499</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex-none text-white hover:text-teal-400 cursor-pointer">
+                    <div className="mx-3 flex">
+                        <FaLocationDot className="xl:mx-4 lg:mx-1 mt-1.5 text-xl text-teal-400"></FaLocationDot>
+                        <div>
+                            <h2 className="xl:text-lg lg:text-md">Masterpara, Feni, Bangladesh</h2>
+                        </div>
+                    </div>
+                </div>
             </div> 
-            <div>
+            <div className="">
                 <header className="footer-title">Services</header> 
                 <a className="link link-hover">Branding</a>
                 <a className="link link-hover">Design</a>
@@ -27,8 +55,10 @@ const Footer = () => {
                 <a className="link link-hover">Privacy policy</a>
                 <a className="link link-hover">Cookie policy</a>
             </div>
+                </div>
             </div>
         </div>
+
     );
 };
 
