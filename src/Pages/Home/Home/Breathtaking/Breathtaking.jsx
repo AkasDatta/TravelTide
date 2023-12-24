0de482d4065b9c15f0885cf66e651551d2dff116
Akas_Datta <012akasdatta@gmail.com>
2023-12-25 03:08:36 +0600

    text added from the breathtaking component

--- src/Pages/Home/Home/Breathtaking/Breathtaking.jsx
+++ src/Pages/Home/Home/Breathtaking/Breathtaking.jsx
@@ -1,8 +1,10 @@
-
 const Breathtaking = () => {
     return (
-        <div>
-            <h2>Go and discover</h2>
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-4 xl:pt-28 lg:pt-28 md:pt-28 py-32">
+            <div className="text-center">
+                <h2 className="text-3xl font-serif text-cyan-500">Go & Discover</h2>
+                <h2 className="text-5xl my-3 font-bold text-gray-900">Breathtaking Cities</h2>
+            </div>
         </div>
     );
 };
