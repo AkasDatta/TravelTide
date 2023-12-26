3f286b76e2e775de7aa4984e15894ad4fac907a0
Akas_Datta <012akasdatta@gmail.com>
2023-12-27 01:17:03 +0600

    carouselCard bg image added

--- src/Pages/Home/Home/CarouselCard/CarouselCard.jsx
+++ src/Pages/Home/Home/CarouselCard/CarouselCard.jsx
@@ -1,16 +1,13 @@
-import image1 from "../../../../assets/shell.png";
-import image2 from "../../../../assets/women_sea.png";
+import image1 from '../../../../assets/shellBanner.png';
+
 const CarouselCard = () => {
-    return (
-        <div className="grid grid-cols-2">
-            <div className="">
-                <img src={image1} alt="" />
-            </div>
-            <div>
-                <img src={image2} alt="" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="">
+      <div className="flex items-center justify-center">
+        <img src={image1} alt="" className="mx-auto my-0" />
+      </div>
+    </div>
+  );
 };
 
 export default CarouselCard;
