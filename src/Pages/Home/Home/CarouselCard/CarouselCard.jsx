d5983d1d3f5eeb99bb905c744e76068f7ce6e65e
Akas_Datta <012akasdatta@gmail.com>
2023-12-27 16:47:53 +0600

    casouselcard component some image and other details added

--- src/Pages/Home/Home/CarouselCard/CarouselCard.jsx
+++ src/Pages/Home/Home/CarouselCard/CarouselCard.jsx
@@ -1,8 +1,20 @@
 import image1 from '../../../../assets/shellBanner.png';
 import plan from '../../../../assets/plan.png';
-import { Carousel } from 'flowbite-react';
+import Rating from 'react-rating';
+import { useEffect, useState } from 'react';
+import { FaRegStar, FaStar } from 'react-icons/fa';
 
 const CarouselCard = () => {
+  const [card, setCard] = useState([]);
+
+  useEffect(() => {
+    fetch('/public/carddetails.json')
+      .then(res => res.json())
+      .then(data => setCard(data))
+      .catch(error => console.log(error))
+  }, []);
+
+
   return (
     <div className="bg-[#F8F8F8]">
       <div className="flex items-center justify-center">
@@ -16,20 +28,41 @@ const CarouselCard = () => {
             <h2 className="text-5xl my-3 font-bold text-gray-900">Perfect Holiday</h2>
             <p className='mt-4 text-lg text-gray-700'>Craft your perfect getaway with our custom travel plans. <br /> Explore handpicked itineraries in our Plan and Holiday section.</p>
         </div>
-      
-
-  
-            <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-              <Carousel slideInterval={5000}>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
-              </Carousel>
-            </div>
-
-
+        <div>
+        {card.map((state, index) => (
+          <a href="#" className="group relative block overflow-hidden" key={index}>
+            <img
+              src={state.image}
+              alt=""
+              className="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+            />
+            <div className="relative pt-32 sm:pt-48 lg:pt-48">
+            <h2 className="block text-2xl font-bold ">{state.name}</h2>
+                <h2 className="text-xl">{state.description}</h2>
+                <div className="">
+                      <div className="flex items-center justify-between  font-bold">
+                        <div className="flex gap-2">
+                            <p>
+                            <Rating
+                                placeholderRating={state.rating}
+                                readonly
+                                emptySymbol={<FaRegStar></FaRegStar>}
+                                placeholderSymbol={<FaStar className='text-danger'></FaStar>}
+                                fullSymbol={<FaStar></FaStar>}
+                            ></Rating>{' '}
+                            </p>
+                              <p>{state.rating}</p>
+                              <p>{state.quality_name}</p>
+                          </div>
+                          <div className="text-right text-xl">
+                              <h2>{state.dollarSign}{state.dollar}</h2>
+                          </div>
+                    </div>
+                </div>
+              </div>    
+          </a>
+          ))}
+        </div>
       </div>
     </div>
   );
