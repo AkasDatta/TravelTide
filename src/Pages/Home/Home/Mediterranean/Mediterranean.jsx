4dc01674a3979b9c90491b7edb6727c1d40c4e16
Akas_Datta <012akasdatta@gmail.com>
2023-12-31 19:56:20 +0600

    margiin padding added for mediterranean component

--- src/Pages/Home/Home/Mediterranean/Mediterranean.jsx
+++ src/Pages/Home/Home/Mediterranean/Mediterranean.jsx
@@ -1,7 +1,7 @@
 import unforgettable from "../../../../assets/unfo.png"
 const Mediterranean = () => {
     return (
-        <div>
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-4 xl:pt-28 lg:pt-28 md:pt-32 pt-32">
             <div className="text-center pb-20">
                 <div className="flex justify-center items-center">
                     <img src={unforgettable} alt="" />
