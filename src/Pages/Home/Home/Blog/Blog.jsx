2672a7cb7ed79de6a4d3f8d1a1a629bc00922494
Akas_Datta <012akasdatta@gmail.com>
2024-01-06 17:44:51 +0600

    blog heading and description added

--- src/Pages/Home/Home/Blog/Blog.jsx
+++ src/Pages/Home/Home/Blog/Blog.jsx
@@ -2,7 +2,8 @@
 const Blog = () => {
     return (
         <div>
-            <h2>This is Blog</h2>
+            <h2>From Our Blog</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic magni officiis sint ea eos. Deserunt, ut aspernatur! Deserunt, provident sunt!</p>
         </div>
     );
 };
