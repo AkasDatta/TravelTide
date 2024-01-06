caafc82102d8c75ef5b28240f4356167e4583b7e
Akas_Datta <012akasdatta@gmail.com>
2024-01-06 23:00:02 +0600

    grid item added and h2 line and description added

--- src/Pages/Home/Home/Blog/Blog.jsx
+++ src/Pages/Home/Home/Blog/Blog.jsx
@@ -1,9 +1,18 @@
 
 const Blog = () => {
     return (
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-4 xl:pb-28 lg:pb-28 md:pb-32 pb-32">
-            <h2>From Our Blog</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic magni officiis sint ea eos. Deserunt, ut aspernatur! Deserunt, provident sunt!</p>
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-4 xl:py-28 lg:py-28 md:py-32 py-32">
+           <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+                <div className="col-span-2">
+                    <h2 className="text-5xl font-bold text-gray-900">From Our Blog</h2>                
+                    <p className='mt-4 text-lg text-gray-700'>Dive into micro-adventures with our tiny travel tales. Big inspiration, small reads. Your passport to quick escapes!</p>
+
+                    
+                </div>
+                <div className="col-span-1">
+                    <h2>jiojhoij</h2>
+                </div>
+           </div>
         </div>
     );
 };
